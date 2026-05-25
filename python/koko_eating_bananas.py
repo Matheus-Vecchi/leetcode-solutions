db95a0a0ffcc76a293af111ce037f8ac80f08213
Matheus-Vecchi <matheusvecchi02@gmail.com>
2026-05-24 22:06:31 -0300

    LeetCode: update python/koko_eating_bananas.py

---
+++ python/koko_eating_bananas.py
@@ -0,0 +1,37 @@
+# ======================================
+# LeetCode Problem: koko eating bananas
+# Language: python3
+# Link: https://leetcode.com/problems/koko-eating-bananas/
+# Synced by: LinkCode
+# Date: 24/05/2026, 22:06:31
+# ======================================
+
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        low = 1
+        high = max(piles)
+        ans = high
+
+        while low < high:
+            mid = (low+high) // 2
+            if self.validAmount(mid, piles, h):
+                ans = mid
+                high = mid
+            else:
+                low = mid + 1
+        
+        return ans
+
+
+    def validAmount(self, mid, piles, h):
+        wasted_hours = 0
+
+        for b in piles:
+            hours = (b + mid - 1) // mid
+            wasted_hours += hours
+        
+        if wasted_hours <= h:
+            return True
+        else:
+            return False
