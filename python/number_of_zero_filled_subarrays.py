caf776ceeac5075088b308d33a7c081c102dbcd2
Matheus-Vecchi <matheusvecchi02@gmail.com>
2026-06-11 22:50:26 -0300

    LeetCode: update python/number_of_zero_filled_subarrays.py

--- python/number_of_zero_filled_subarrays.py
+++ python/number_of_zero_filled_subarrays.py
@@ -3,7 +3,7 @@
 # Language: python3
 # Link: https://leetcode.com/problems/number-of-zero-filled-subarrays/
 # Synced by: LinkCode
-# Date: 11/06/2026, 22:50:15
+# Date: 11/06/2026, 22:50:26
 # ======================================
 
 
