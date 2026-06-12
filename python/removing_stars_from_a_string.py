602e54ad7055873430d77092a47c36b749c22094
Matheus-Vecchi <matheusvecchi02@gmail.com>
2026-06-12 15:13:54 -0300

    LeetCode: update python/removing_stars_from_a_string.py

---
+++ python/removing_stars_from_a_string.py
@@ -0,0 +1,20 @@
+# ======================================
+# LeetCode Problem: removing stars from a string
+# Language: python3
+# Link: https://leetcode.com/problems/removing-stars-from-a-string/
+# Synced by: LinkCode
+# Date: 12/06/2026, 15:13:53
+# ======================================
+
+
+class Solution:
+    def removeStars(self, s: str) -> str:
+        stack = []
+
+        for i in s:
+            if i != "*":
+                stack.append(i)
+            else:
+                stack.pop()
+        
+        return "".join(stack)
