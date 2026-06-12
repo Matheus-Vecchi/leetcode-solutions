9ab04bed796b41a9ce502c79d5806a695e40cf16
Matheus-Vecchi <matheusvecchi02@gmail.com>
2026-06-11 22:30:46 -0300

    LeetCode: update python/number_of_zero_filled_subarrays.py

---
+++ python/number_of_zero_filled_subarrays.py
@@ -0,0 +1,25 @@
+# ======================================
+# LeetCode Problem: number of zero filled subarrays
+# Language: python3
+# Link: https://leetcode.com/problems/number-of-zero-filled-subarrays/
+# Synced by: LinkCode
+# Date: 11/06/2026, 22:30:46
+# ======================================
+
+
+class Solution:
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+        l = 0
+        ans = 0
+
+        for r in range(len(nums)):
+            if nums[r] == 0:
+                if nums[l] != 0:
+                    l = r
+            
+                ans += r - l + 1
+            else:
+                l = r
+        
+        return ans
+                
