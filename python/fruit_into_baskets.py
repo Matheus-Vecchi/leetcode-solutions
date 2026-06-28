084857ce880e8320dd04d648a3e2a08da13028ae
Matheus-Vecchi <matheusvecchi02@gmail.com>
2026-06-28 00:28:01 -0300

    LeetCode: update python/fruit_into_baskets.py

--- python/fruit_into_baskets.py
+++ python/fruit_into_baskets.py
@@ -3,31 +3,32 @@
 # Language: python3
 # Link: https://leetcode.com/problems/fruit-into-baskets/
 # Synced by: LinkCode
-# Date: 17/05/2026, 17:08:51
+# Date: 28/06/2026, 00:28:00
 # ======================================
 
 
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
         l = 0
-        seq = {}
-        ans = 1
+        empty_baskets = 2
+        basket = {}
+        ans = 0
 
         for r in range(len(fruits)):
-            if fruits[r] in seq:
-                seq[fruits[r]] += 1
+            if fruits[r] not in basket:
+                basket[fruits[r]] = 1
+                empty_baskets -= 1
             else:
-                seq[fruits[r]] = 1
-
-            while len(seq) > 2:
-                if seq[fruits[l]] == 1:
-                    seq.pop(fruits[l])
-                else:
-                    seq[fruits[l]] -= 1
-                l += 1
+                basket[fruits[r]] += 1
             
-            ans = max(ans, r - l + 1)
-        
+            while empty_baskets < 0:
+                if basket[fruits[l]] == 1:
+                    del basket[fruits[l]]
+                    empty_baskets += 1
+                else:
+                    basket[fruits[l]] -= 1
+                l += 1
+
+            ans = max(ans, r - l  + 1)
+
         return ans
-
-
