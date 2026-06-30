ca316a2e26e131f07bbeec4d965be135cd0d4660
Matheus-Vecchi <matheusvecchi02@gmail.com>
2026-06-29 23:12:04 -0300

    LeetCode: update python/count_good_nodes_in_binary_tree.py

--- python/count_good_nodes_in_binary_tree.py
+++ python/count_good_nodes_in_binary_tree.py
@@ -3,7 +3,7 @@
 # Language: python3
 # Link: https://leetcode.com/problems/count-good-nodes-in-binary-tree/
 # Synced by: LinkCode
-# Date: 29/06/2026, 23:10:40
+# Date: 29/06/2026, 23:12:04
 # ======================================
 
 
@@ -16,20 +16,21 @@
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
         max_seen = root.val
-        ans = 0
 
         def dfs(root, max_seen):
-            nonlocal ans
-
             if not root:
                 return 0
             
             if root.val >= max_seen:
-                ans += 1
+                ans = 1
                 max_seen = root.val
+            else:
+                ans = 0
             
-            dfs(root.left, max_seen)
-            dfs(root.right, max_seen)
+            left = dfs(root.left, max_seen)
+            right = dfs(root.right, max_seen)
         
-        dfs(root, max_seen)
-        return ans
+            return left + right + ans
+        
+        return dfs(root, max_seen)
+            
