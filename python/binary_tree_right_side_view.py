d53b7cf425700eab16652482073f4c2c1d5a69ac
Matheus-Vecchi <matheusvecchi02@gmail.com>
2026-06-29 23:07:57 -0300

    LeetCode: update python/binary_tree_right_side_view.py

--- python/binary_tree_right_side_view.py
+++ python/binary_tree_right_side_view.py
@@ -3,7 +3,7 @@
 # Language: python3
 # Link: https://leetcode.com/problems/binary-tree-right-side-view/
 # Synced by: LinkCode
-# Date: 21/06/2026, 22:39:35
+# Date: 29/06/2026, 23:07:57
 # ======================================
 
 
@@ -13,24 +13,22 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        ans = []
-
         queue = deque()
+        ans = []
 
         if root:
             queue.append(root)
         
         while queue:
             ans.append(queue[-1].val)
-            for i in range(1, len(queue)+1):
+            for _ in range(len(queue)):
                 curr = queue.popleft()
 
                 if curr.left:
                     queue.append(curr.left)
                 if curr.right:
                     queue.append(curr.right)
-            
-        return list(ans)
+        
+        return ans
