9990d56d2cbe3a4f064774ffc52b2a68092a9c78
Matheus-Vecchi <matheusvecchi02@gmail.com>
2026-07-03 18:05:04 -0300

    LeetCode: update python/merge_two_binary_trees.py

--- python/merge_two_binary_trees.py
+++ python/merge_two_binary_trees.py
@@ -3,7 +3,7 @@
 # Language: python3
 # Link: https://leetcode.com/problems/merge-two-binary-trees/
 # Synced by: LinkCode
-# Date: 03/07/2026, 17:51:56
+# Date: 03/07/2026, 18:05:04
 # ======================================
 
 
@@ -15,27 +15,16 @@
 #         self.right = right
 class Solution:
     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
-        root3 = TreeNode(0)
+        if not root1 and not root2:
+            return None
+        if not root1:
+            return root2
+        if not root2:
+            return root1
 
-        def dfs(root1, root2, root3):
-            if not root1 and not root2:
-                return None
-
-            if root1 and root2:
-                root3 = TreeNode(root1.val + root2.val)
-                root3.left = dfs(root1.left, root2.left, root3.left)
-                root3.right = dfs(root1.right, root2.right, root3.right)
-            
-            if not root1:
-                root3 = TreeNode(root2.val)
-                root3.left = dfs(None, root2.left, root3.left)
-                root3.right = dfs(None, root2.right, root3.right)
-            if not root2:
-                root3 = TreeNode(root1.val)
-                root3.left = dfs(root1.left, None, root3.left)
-                root3.right = dfs(root1.right, None, root3.right)              
-            
-            return root3
-            
-        return dfs(root1, root2, root3)
+        root1.val += root2.val
         
+        root1.left = self.mergeTrees(root1.left, root2.left)
+        root1.right = self.mergeTrees(root1.right, root2.right)
+
+        return root1
