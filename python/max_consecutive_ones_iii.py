23a971e219087205eaf0762fb930d80146903a41
Matheus-Vecchi <matheusvecchi02@gmail.com>
2026-07-12 14:23:41 -0300

    LeetCode: update python/max_consecutive_ones_iii.py

--- python/max_consecutive_ones_iii.py
+++ python/max_consecutive_ones_iii.py
@@ -3,27 +3,24 @@
 # Language: python3
 # Link: https://leetcode.com/problems/max-consecutive-ones-iii/
 # Synced by: LinkCode
-# Date: 27/06/2026, 23:58:45
+# Date: 12/07/2026, 14:23:40
 # ======================================
 
 
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        l = 0
-        lifes = k
         ans = 0
+        count = 0
 
+        l = 0
         for r in range(len(nums)):
             if nums[r] == 0:
-                lifes -= 1
-            
-            while lifes < 0:
-                if nums[l] == 0:
-                    lifes += 1
-                l += 1
+                count += 1
+                while count > k:
+                    if nums[l] == 0:
+                        count -= 1
+                    l += 1
 
             ans = max(ans, r - l + 1)
-
-
-        return ans
         
+        return ans
