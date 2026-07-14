14198a839035173403bd86fe3a624801289f94ce
Matheus-Vecchi <matheusvecchi02@gmail.com>
2026-07-13 23:52:48 -0300

    LeetCode: update python/path_sum_iii.py

--- python/path_sum_iii.py
+++ python/path_sum_iii.py
@@ -3,7 +3,7 @@
 # Language: python3
 # Link: https://leetcode.com/problems/path-sum-iii/
 # Synced by: LinkCode
-# Date: 13/07/2026, 19:00:02
+# Date: 13/07/2026, 23:52:48
 # ======================================
 
 
@@ -15,31 +15,26 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        if not root:
-            return 0
-
-        valid_paths = self.dfs(root, 0, targetSum)
-
-        left = self.pathSum(root.left, targetSum)
-        right = self.pathSum(root.right, targetSum)
-
-        return valid_paths + right + left
-
-
-    def dfs(self, root, current_sum, targetSum):
-        if not root:
-            return 0
         
-        current_sum += root.val
-        if current_sum == targetSum:
-            count = 1
-        else:
-            count = 0
-        
-        left = self.dfs(root.left, current_sum, targetSum)
-        right = self.dfs(root.right, current_sum, targetSum)
+        def dfs(root, current_sum, hashmap):
+            if not root:
+                return 0
+            
+            current_sum += root.val
+            if current_sum - targetSum in hashmap:
+                count = hashmap[current_sum - targetSum]
+            else:
+                count = 0
+            if current_sum in hashmap:
+                hashmap[current_sum] += 1
+            else:
+                hashmap[current_sum] = 1
+            
+            left = dfs(root.left, current_sum, hashmap)
+            right = dfs(root.right, current_sum, hashmap)
 
-        if count != 0:
-            return 1 + left + right
-        else:
-            return left + right
+            hashmap[current_sum] -= 1
+
+            return count + left + right
+        
+        return dfs(root, 0, {0: 1})
