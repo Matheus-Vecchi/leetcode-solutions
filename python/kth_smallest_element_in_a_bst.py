2ff3746728963694246b2d65ac5751d97ab77cef
Matheus-Vecchi <matheusvecchi02@gmail.com>
2026-07-15 20:20:22 -0300

    LeetCode: update python/kth_smallest_element_in_a_bst.py

--- python/kth_smallest_element_in_a_bst.py
+++ python/kth_smallest_element_in_a_bst.py
@@ -3,7 +3,7 @@
 # Language: python3
 # Link: https://leetcode.com/problems/kth-smallest-element-in-a-bst/
 # Synced by: LinkCode
-# Date: 18/06/2026, 18:14:29
+# Date: 15/07/2026, 20:20:22
 # ======================================
 
 
@@ -13,24 +13,16 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        count = 0
-        res = None
-
-        def bst(root, k):
-            nonlocal count, res
+        
+        def dfs(root):
             if not root:
-                return None
+                return []
             
-            bst(root.left, k)
-            count += 1
-            if count == k:
-                res = root.val
-                return
-            bst(root.right, k)
+            left = dfs(root.left)
+            right = dfs(root.right)
+
+            return left + [root.val] + right
         
-        bst(root, k)
-        return res
-        
+        return dfs(root)[k-1]
