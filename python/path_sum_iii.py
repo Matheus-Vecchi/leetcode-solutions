7153a7c78f25422f69373cc9505e7f750b1beed0
Matheus-Vecchi <matheusvecchi02@gmail.com>
2026-07-22 18:08:12 -0300

    LeetCode: update python/path_sum_iii.py

--- python/path_sum_iii.py
+++ python/path_sum_iii.py
@@ -3,7 +3,7 @@
 # Language: python3
 # Link: https://leetcode.com/problems/path-sum-iii/
 # Synced by: LinkCode
-# Date: 13/07/2026, 23:52:48
+# Date: 22/07/2026, 18:08:12
 # ======================================
 
 
@@ -15,26 +15,29 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        
-        def dfs(root, current_sum, hashmap):
+        prefix = {0: 1}
+
+        def dfs(root, acc, prefix):
             if not root:
                 return 0
             
-            current_sum += root.val
-            if current_sum - targetSum in hashmap:
-                count = hashmap[current_sum - targetSum]
-            else:
-                count = 0
-            if current_sum in hashmap:
-                hashmap[current_sum] += 1
-            else:
-                hashmap[current_sum] = 1
+            count = 0
             
-            left = dfs(root.left, current_sum, hashmap)
-            right = dfs(root.right, current_sum, hashmap)
+            acc += root.val
 
-            hashmap[current_sum] -= 1
+            if acc - targetSum in prefix:
+                count = prefix[acc - targetSum]
 
-            return count + left + right
-        
-        return dfs(root, 0, {0: 1})
+            if acc not in prefix:
+                prefix[acc] = 1
+            else:
+                prefix[acc] += 1
+
+            left = dfs(root.left, acc, prefix)
+            right = dfs(root.right, acc, prefix)
+
+            prefix[acc] -= 1
+
+            return left + right + count
+
+        return dfs(root, 0, prefix)
