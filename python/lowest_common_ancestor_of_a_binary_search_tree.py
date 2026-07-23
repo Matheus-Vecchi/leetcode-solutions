514cd7003d80145e0ab8cb7cf960529b45b37eba
Matheus-Vecchi <matheusvecchi02@gmail.com>
2026-07-23 12:56:21 -0300

    LeetCode: update python/lowest_common_ancestor_of_a_binary_search_tree.py

--- python/lowest_common_ancestor_of_a_binary_search_tree.py
+++ python/lowest_common_ancestor_of_a_binary_search_tree.py
@@ -3,7 +3,7 @@
 # Language: python3
 # Link: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
 # Synced by: LinkCode
-# Date: 23/07/2026, 12:47:21
+# Date: 23/07/2026, 12:56:21
 # ======================================
 
 
@@ -16,25 +16,13 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        
-        def dfs(root, p, q):
-            if not root:
-                return
-            if root == p:
-                return p
-            if root == q:
-                return q
-            
-            left = None
-            right = None
-
-            if root.val > p.val and root.val > q.val:
-                left = dfs(root.left, p, q)
-            elif root.val < p.val and root.val < q.val:
-                right = dfs(root.right, p, q)
+        curr = root
+        while curr:
+            if curr == p or curr == q:
+                return curr
+            if curr.val > p.val and curr.val > q.val:
+                curr = curr.left
+            elif curr.val < p.val and curr.val < q.val:
+                curr = curr.right
             else:
-                return root
-
-            return left or right
-        
-        return dfs(root, p, q)
+                return curr
