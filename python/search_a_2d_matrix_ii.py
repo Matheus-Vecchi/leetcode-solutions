7baad986c19c7779929a462d8e4120d531bf0af7
Matheus-Vecchi <matheusvecchi02@gmail.com>
2026-07-26 12:54:40 -0300

    LeetCode: update python/search_a_2d_matrix_ii.py

--- python/search_a_2d_matrix_ii.py
+++ python/search_a_2d_matrix_ii.py
@@ -3,17 +3,17 @@
 # Language: python3
 # Link: https://leetcode.com/problems/search-a-2d-matrix-ii/
 # Synced by: LinkCode
-# Date: 26/07/2026, 12:53:20
+# Date: 26/07/2026, 12:54:40
 # ======================================
 
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        for row in range(len(matrix)):
+        for row in range(len(matrix)): # m
             l = 0
             r = len(matrix[row]) - 1
 
-            while l <= r:
+            while l <= r: # log n
                 mid = (l+r) // 2
 
                 if matrix[row][mid] == target:
@@ -24,3 +24,5 @@ class Solution:
                     l = mid + 1
             
         return False
+# time: O(m log n)
+# space: O(1)
