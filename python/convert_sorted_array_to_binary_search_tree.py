242fa45976cded469107feb3d438b0b7fb817313
Matheus-Vecchi <matheusvecchi02@gmail.com>
2026-07-28 12:50:53 -0300

    LeetCode: update python/convert_sorted_array_to_binary_search_tree.py

--- python/convert_sorted_array_to_binary_search_tree.py
+++ python/convert_sorted_array_to_binary_search_tree.py
@@ -3,7 +3,7 @@
 # Language: python3
 # Link: https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
 # Synced by: LinkCode
-# Date: 28/07/2026, 12:48:54
+# Date: 28/07/2026, 12:50:55
 # ======================================
 
 
@@ -16,16 +16,16 @@
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
         
-        def dfs(root, lowest, highest):
+        def dfs(lowest, highest):
             if lowest > highest:
                 return None
 
             mid = (lowest + highest) // 2
             root = TreeNode(nums[mid])
 
-            root.left = dfs(root.left, lowest, mid - 1)
-            root.right = dfs(root.right, mid + 1, highest)
+            root.left = dfs(lowest, mid - 1)
+            root.right = dfs(mid + 1, highest)
 
             return root
         
-        return dfs(TreeNode(nums[(0 + len(nums) - 1) // 2]), 0, len(nums) - 1)
+        return dfs(0, len(nums) - 1)
