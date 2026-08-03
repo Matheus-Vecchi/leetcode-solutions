33b55cbc99955f7021f754f2c364c67a0123cdf3
Matheus-Vecchi <matheusvecchi02@gmail.com>
2026-08-02 23:22:36 -0300

    LeetCode: update python/add_two_numbers_ii.py

--- python/add_two_numbers_ii.py
+++ python/add_two_numbers_ii.py
@@ -3,7 +3,7 @@
 # Language: python3
 # Link: https://leetcode.com/problems/add-two-numbers-ii/
 # Synced by: LinkCode
-# Date: 02/08/2026, 22:25:04
+# Date: 02/08/2026, 23:22:36
 # ======================================
 
 
@@ -14,63 +14,63 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        curr1 = l1
-        prev1 = None
-        curr2 = l2
-        prev2 = None
+        stack1 = []
+        stack2 = []
+        ans = []
 
-        while curr1:
-            aux = curr1.next
-            curr1.next = prev1
-            prev1 = curr1
-            curr1 = aux
-        l1 = prev1
-        
-        while curr2:
-            aux = curr2.next
-            curr2.next = prev2
-            prev2 = curr2
-            curr2 = aux
-        l2 = prev2
-
-        dummy = ListNode(0)
-        curr = dummy
-        carry = 0
-        while l1 or l2 or carry == 1:
-            if l1:
+        while l1 or l2:
+            if l1 is not None:
                 v1 = l1.val
             else:
-                v1 = 0
-            if l2:
+                v1 = None
+            if l2 is not None:
                 v2 = l2.val
             else:
-                v2 = 0
-
-            result = v1 + v2 + carry
-
-            if result > 9:
-                result -= 10
-                carry = 1
-            else:
-                carry = 0
+                v2 = None
             
-            curr.next = ListNode(result)
-            curr = curr.next
+            if v1 is not None:
+                stack1.append(v1)
+            if v2 is not None:
+                stack2.append(v2)
+            
             if l1:
                 l1 = l1.next
             if l2:
                 l2 = l2.next
         
-        curr.next = l1 or l2
-    
+        carry = 0
+        while stack1 or stack2 or carry == 1:
+            if stack1:
+                s1 = stack1[-1]
+            else:
+                s1 = 0
+            if stack2:
+                s2 = stack2[-1]
+            else:
+                s2 = 0
+
+            result = s1 + s2 + carry
+
+            if stack1:
+                stack1.pop()
+            if stack2:
+                stack2.pop()
+            
+            if result > 9:
+                ans.append(result - 10)
+                carry = 1                    
+            else:
+                ans.append(result)
+                carry = 0
         
+        dummy = ListNode(0)
+        curr = dummy
 
-        curr = dummy.next
-        prev = None
-        while curr:
-            aux = curr.next
-            curr.next = prev
-            prev = curr
-            curr = aux
+        stop = len(ans) * (-1)
 
-        return prev
+        for i in range(1, len(ans)+1):
+            curr.next = ListNode(ans[-i])
+            curr = curr.next
+
+        return dummy.next
+  
