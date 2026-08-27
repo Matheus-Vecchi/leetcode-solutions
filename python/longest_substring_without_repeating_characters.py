1b0fd29af7b67245a452167dbcbcd843e718eb23
Matheus-Vecchi <matheusvecchi02@gmail.com>
2026-08-27 13:43:13 -0300

    LeetCode: update python/longest_substring_without_repeating_characters.py

--- python/longest_substring_without_repeating_characters.py
+++ python/longest_substring_without_repeating_characters.py
@@ -3,23 +3,23 @@
 # Language: python3
 # Link: https://leetcode.com/problems/longest-substring-without-repeating-characters/
 # Synced by: LinkCode
-# Date: 04/06/2026, 23:20:00
+# Date: 27/08/2026, 13:43:14
 # ======================================
 
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        l = 0
         hashset = set()
         ans = 0
 
+        l = 0
         for r in range(len(s)):
             while s[r] in hashset:
                 hashset.remove(s[l])
                 l += 1
-        
-            ans = max(ans, r - l + 1)            
+            
             hashset.add(s[r])
+            
+            ans = max(ans, r - l + 1)
         
         return ans
-        
