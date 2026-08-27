725b80e3bd757e2741a24f11aa547cd540feee9a
Matheus-Vecchi <matheusvecchi02@gmail.com>
2026-08-27 13:52:23 -0300

    LeetCode: update python/longest_substring_without_repeating_characters.py

--- python/longest_substring_without_repeating_characters.py
+++ python/longest_substring_without_repeating_characters.py
@@ -3,7 +3,7 @@
 # Language: python3
 # Link: https://leetcode.com/problems/longest-substring-without-repeating-characters/
 # Synced by: LinkCode
-# Date: 27/08/2026, 13:49:25
+# Date: 27/08/2026, 13:52:23
 # ======================================
 
 
@@ -16,7 +16,6 @@ class Solution:
         for r in range(len(s)):
             if s[r] in hashmap and hashmap[s[r]] >= l:
                 l = hashmap[s[r]] + 1
-                hashmap[s[r]] = r
             
             hashmap[s[r]] = r
             
